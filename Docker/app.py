8a0df2d6cba377c7364526e899b09aabb340192f
Martin <matkodevops@gmail.com>
2024-10-03 14:16:58 +0200

    Add dockerfile and requirements

---
+++ Docker/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, send_from_directory, send_file, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+def create_database():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task TEXT)''')
+    conn.commit()
+    conn.close()
+
+
+@app.route("/lol")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/todo')
+def todo():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM tasks')
+    tasks = c.fetchall()
+    conn.close()
+    return render_template('todo.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form['task']
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO tasks (task) VALUES (?)', (task,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('todo'))
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('todo'))
+
+@app.route('/download')
+def download():
+    path = 'file.txt'
+    return send_file(path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    create_database()
+    app.run(debug=True, host='0.0.0.0', port=5000)
